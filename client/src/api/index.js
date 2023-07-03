09366f6fb028853292eeea203627f249f58e476f
Kevin Chen <chenkevin711@gmail.com>
2023-07-03 16:45:08 -0400

    add vercel json

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://mern-blog-silk.vercel.app' })
+const API = axios.create({ baseURL: 'https://mern-blog-api-gamma.vercel.app' })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
